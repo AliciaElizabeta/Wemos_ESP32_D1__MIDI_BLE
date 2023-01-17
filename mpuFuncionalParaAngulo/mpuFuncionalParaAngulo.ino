b8808a4317048a1be096620b6d4b12d1eb449fbb
Alicia Elizabeta Marrero Ravelo <72342937+AliciaElizabeta@users.noreply.github.com>
2023-01-17 12:10:14 +0000

    Transforma los valores angulares en notas
    A partir del metodo map recibe un rango angular y devuelve un valor de nota añadido a la nota base para generar la escala Do4 -> Si4 en valor de nota MIDI

--- mpuFuncionalParaAngulo/mpuFuncionalParaAngulo.ino
+++ mpuFuncionalParaAngulo/mpuFuncionalParaAngulo.ino
@@ -10,7 +10,7 @@ MPU6050 sensor;
 
 // Valores RAW (sin procesar) del acelerometro  en los ejes x,y,z
 short ax, ay, az;
-
+int minNote = 60; // In MIDI 60 is equivalent to C or Do
 void setup() {
   Serial.begin(57600);    //Iniciando puerto serial
   Wire.begin();           //Iniciando I2C  
@@ -26,10 +26,55 @@ void loop() {
   //Calcular los angulos de inclinacion:
   float accel_ang_x=atan(ax/sqrt(pow(ay,2) + pow(az,2)))*(180.0/3.14);
   float accel_ang_y=atan(ay/sqrt(pow(ax,2) + pow(az,2)))*(180.0/3.14);
+
+  int valueNote = map(accel_ang_x, 80, -80, 0, 11);
+  int note = minNote + valueNote;
+
+  //Este bloque es solo para visualizar el correcto funcionamiento
+  // basta con enviar el midi con note 
+  switch (note) {
+    case 60:
+      Serial.println("DO");
+      break;
+    case 61:
+      Serial.println("Do#");
+      break;
+    case 62:
+      Serial.println("RE");
+      break;
+    case 63:
+      Serial.println("Re#");
+      break;
+    case 64:
+      Serial.println("MI");
+      break;
+    case 65:
+      Serial.println("FA");
+      break;
+    case 66:  
+      Serial.println("Fa#");
+      break;
+    case 67:  
+      Serial.println("SOL");
+      break;
+    case 68:
+      Serial.println("Sol#");
+      break;
+    case 69:
+      Serial.println("LA");
+      break;
+    case 70:  
+      Serial.println("La#");
+      break;
+    case 71:  
+      Serial.println("SI");
+      break;
+  }
+
   //Mostrar los angulos separadas por un [tab]
-  Serial.print("Inclinacion en X: ");
+  Serial.print("   Inclinacion en X: ");
   Serial.print(accel_ang_x); 
-  Serial.print("tInclinacion en Y:");
+  Serial.print("   Inclinacion en Y:");
   Serial.println(accel_ang_y);
   delay(100);
 }
